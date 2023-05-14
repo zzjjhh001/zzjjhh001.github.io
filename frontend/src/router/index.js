26efa4ea39001c7e346ae50d6382a5d0c3ed71c5
zhaojinghui <952488120@qq.com>
2023-05-14 16:28:08 +0800

    删除无用的组件

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,14 +19,6 @@ const router = createRouter({
       name: 'name',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
     
   ]
 })
