4c024131c2d062ea786f01b8684639662bde4602
zzjjhh001 <952488120@qq.com>
2024-01-06 15:28:10 +0800

    init

--- packages/disk/src/views/Disk/index.tsx
+++ packages/disk/src/views/Disk/index.tsx
@@ -1,10 +1,13 @@
 import { defineComponent } from "vue";
-
+import styles from './index.module.scss';
 const component = defineComponent({
-  name: '',
+  name: 'DiskView',
   setup() {
-
+    return () => (
+      <div class={styles.main}>
+        <div>qwewq</div>
+      </div>
+    )
   },
-}
-));
+});
 export default component;
